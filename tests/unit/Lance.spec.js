7de909523776b67801fa81330745608cccf09bcb
Pedro <pedromiguelnevesdemeu@gmail.com>
2021-09-07 13:29:31 -0300

    descrevendo casos de teste

--- tests/unit/Lance.spec.js
+++ tests/unit/Lance.spec.js
@@ -30,3 +30,13 @@ test('Emite um lance quando o valor é maior do que 0', () => {
 
     expect(lancesEmitidos).toHaveLength(1)
 })
+
+test('Emite o valor esperado de um lance válido', () => {
+    const wrapper = mount(Lance)
+    const input = wrapper.find('input')
+    input.setValue(100)
+    wrapper.trigger('submit')
+    const lancesEmitidos = wrapper.emitted('novo-lance')
+    const lance = parseInt(lancesEmitidos[0][0])
+    expect(lance).toBe(100)
+})
