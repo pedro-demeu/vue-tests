7f1346c16939ba03d75deb28a6c376d016cbf276
Pedro <pedromiguelnevesdemeu@gmail.com>
2021-09-07 19:18:11 -0300

    trabalhando com props

--- tests/unit/Lance.spec.js
+++ tests/unit/Lance.spec.js
@@ -40,3 +40,31 @@ test('Emite o valor esperado de um lance válido', () => {
     const lance = parseInt(lancesEmitidos[0][0])
     expect(lance).toBe(100)
 })
+
+describe('Um lance com valor mínimo', () => {
+    test('todos os lances devem possuir um valor maior do que o mínimo informado', ()=>{
+        const wrapper = mount(Lance, {
+            propsData:{
+                lanceMinimo: 300
+            }
+        })
+        const input = wrapper.find('input')
+        input.setValue(400)
+        wrapper.trigger('submit')
+        const lancesEmitidos = wrapper.emitted('novo-lance')
+        expect(lancesEmitidos).toHaveLength(1)
+    })
+    test('emite o valor esperado de um lance válido', () => {
+        const wrapper = mount(Lance, {
+            propsData:{
+                lanceMinimo: 300
+            }
+        })
+        const input = wrapper.find('input')
+        input.setValue(400)
+        wrapper.trigger('submit')
+        const lancesEmitidos = wrapper.emitted('novo-lance')
+        const valorLance = parseInt(lancesEmitidos[0][0])
+        expect(valorLance).toBe(400)
+    })
+})
