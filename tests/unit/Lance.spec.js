a841b7d1f0b727a7a400f427edc59c07c67e81f7
Pedro <pedromiguelnevesdemeu@gmail.com>
2021-09-07 12:52:57 -0300

    montando componente e verificando se funciona

---
+++ tests/unit/Lance.spec.js
@@ -0,0 +1,9 @@
+import Lance from '@/components/Lance'
+import { mount } from '@vue/test-utils'
+test('Não aceita lance com valor menor do que 0', () => {
+    //expect(true).toBe(true) - usado para testar se a função está sendo chamada
+
+    //Monta um componente e testa se funcionou
+    const wrapper = mount(Lance)
+    expect(wrapper).toBeTruthy()
+})
