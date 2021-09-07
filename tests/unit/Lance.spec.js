59544d389e076c23fb8e1378df8cd2f726048daa
Pedro <pedromiguelnevesdemeu@gmail.com>
2021-09-07 13:06:05 -0300

    testando as saídas

--- tests/unit/Lance.spec.js
+++ tests/unit/Lance.spec.js
@@ -1,14 +1,32 @@
 import Lance from '@/components/Lance'
 import { mount } from '@vue/test-utils'
-test('Não aceita lance com valor menor do que 0', () => {
-    //expect(true).toBe(true) - usado para testar se a função está sendo chamada
 
-    //Monta um componente e testa se funcionou
+test('Não aceita lance com valor menor do que 0', () => {
+
     const wrapper = mount(Lance)
 
     const input = wrapper.find('input')
 
     input.setValue(-100)
 
-    expect(input).toBeTruthy()
+    const lancesEmitidos = wrapper.emitted('novo-lance')
+
+    wrapper.trigger('submit')
+
+    expect(lancesEmitidos).toBeUndefined()
+})
+
+test('Emite um lance quando o valor é maior do que 0', () => {
+
+    const wrapper = mount(Lance)
+
+    const input = wrapper.find('input')
+
+    input.setValue(100)
+
+    wrapper.trigger('submit')
+
+    const lancesEmitidos = wrapper.emitted('novo-lance')
+
+    expect(lancesEmitidos).toHaveLength(1)
 })
