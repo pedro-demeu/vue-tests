2c2c5956ac106bb398e03c457a516fddb78d4128
Pedro <pedromiguelnevesdemeu@gmail.com>
2021-09-07 21:13:58 -0300

    testando avaliador

---
+++ tests/unit/Avaliador.spec.js
@@ -0,0 +1,36 @@
+import Avaliador from '@/views/Avaliador'
+import { mount, RouterLinkStub } from '@vue/test-utils'
+import { getLeiloes } from '@/http'
+import flushPromises from 'flush-promises'
+
+
+jest.mock('@/http')
+
+const leiloes = [
+    {
+        produto: 'Livro da casa do código',
+        lanceInicial: 50,
+        descricao: 'Um livro excelente de REACT'
+    },
+    {
+        produto: 'Livro da casa do código',
+        lanceInicial: 50,
+        descricao: 'Um livro excelente de ANGULAR'
+    },
+]
+
+describe('Um avaliador que se conecta com a API', () => {
+    test('mostra todos os leilões retornados pela API', async () => {
+        getLeiloes.mockResolvedValueOnce(leiloes)
+        const wrapper = mount(Avaliador, {
+            stubs: {
+                RouterLink: RouterLinkStub
+            }
+        })
+
+        await flushPromises()
+
+        const totalLeiloesExibidos = wrapper.findAll('.leilao').length
+        expect(totalLeiloesExibidos).toBe(leiloes.length)
+    })
+})
